a644cf1b8f0568fd0cc9eccf18a67436a5de0e83
Gyan Prakash Singh <113813795+GyanPrakashSinghh@users.noreply.github.com>
2025-01-01 09:49:15 +0530

    Update Resume.jsx

--- src/sections/Resume.jsx
+++ src/sections/Resume.jsx
@@ -9,7 +9,7 @@ const Resume = () => (
   <section  className="p-4">
     <h2 className="text-2xl mb-2">Resume</h2>
     <a
-      href="public/assets/Resume.pdf"
+      href="https://drive.google.com/file/d/1NtdXDSJbUDycdTvIMfOPv8L8ceKQ41dp/view?usp=drive_link"
       download
       className="border border-white text-white py-2 px-4 rounded-lg hover:bg-white hover:text-black transition duration-300"
     >
